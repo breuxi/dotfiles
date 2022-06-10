bfb4f62397f1cd41160b5b43a9628895e1343dea
Breuxi <breuxi@breuxi.de>
2022-06-10 20:00:07 +0200

    Fixed APT Detection & Home directory path

--- deps.sh
+++ deps.sh
@@ -4,7 +4,7 @@ APT_CMD=$(which apt)
 PACMAN_CMD=$(which pacman)
 BREW_CMD=$(which brew)
 
-if [[ ! -z $APT ]]; then
+if [[ ! -z $APT_CMD ]]; then
     sudo apt install zsh
 elif [[ ! -z $PACMAN_CMD ]]; then
     sudo pacman -S zsh
@@ -16,14 +16,15 @@ else
 fi
 
 
-if [ ! -d ~/.oh-my-zsh ]; then
+if [ ! -d $HOME/.oh-my-zsh ]; then
     echo "Installing Oh-my-Zsh!"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    
-    if [ ! -d $ZSH/custom/themes/spaceship-prompt ]; then
-	    echo "Installing Spaceship Prompt!"
-	    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-	    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-    fi
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"    
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt ]; then
+    echo "Installing Spaceship Prompt!"
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt" --depth=1
+    ln -s "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme"
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
 fi
 
